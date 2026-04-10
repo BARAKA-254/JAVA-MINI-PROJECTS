0c5de441369424caa45872f8e880de9db396b314
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-10 16:39:06 +0300

    COMPLETE THE MAIN METHOD BY ACCEPTING USER CHOICE FOR OPERATION AND VALUES OF OPERANDS

--- CALCULATOR/src/Main.java
+++ CALCULATOR/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String operator;
+        double num1;
+        double num2;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("************************************");
+        System.out.println("**WELCOME TO ARITHMETIC CALCULATOR**");
+        System.out.println("************************************");
+
+        System.out.print("Choose one of the following operations: (+,-,*,): ");
+        operator = scanner.nextLine();
+        System.out.print("Enter the first number: ");
+        num1 = scanner.nextDouble();
+        System.out.print("Enter the second number: ");;
+        num2 = scanner.nextDouble();
+
+        scanner.close();
     }
 }

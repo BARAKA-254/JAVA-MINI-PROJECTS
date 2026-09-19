52f797d8243e4a3b6ba9ff5b4187824c0838470c
BARAKA-254 <201593787+BARAKA-254@users.noreply.github.com>
2026-09-19 12:09:39 +0300

    moving the class objects from the local scopes to the class scope

--- BankingProgram/src/Main.java
+++ BankingProgram/src/Main.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 class Main {
-
+  static Scanner scanner = new Scanner(System.in);
   public static void main(String[] args) {
     System.out.println("*****BANKING PROGRAM*****"); 
-    Scanner scanner = new Scanner(System.in);
     double balance = 0;
     int option = 0;
     while(option != 4) { 
@@ -26,7 +25,7 @@ class Main {
                      balance -= withdraw(balance);
                      System.out.println("Balance: " + balance);
                   }
-        case 4 -> System.out.println("exitting!!");
+        case 4 -> System.out.println("exiting!!");
         default -> System.out.println("Invalid option");
       }  
     }  
@@ -37,7 +36,6 @@ class Main {
   }
 
   static double deposit(){
-    Scanner scanner = new Scanner(System.in);
     System.out.print("Amount to deposit: ");
     double amount = scanner.nextDouble();
 
@@ -49,9 +47,8 @@ class Main {
     System.out.println("You have deposited " + amount );
     return amount; 
   }
-  
+
   static double withdraw(double balance){
-    Scanner scanner = new Scanner(System.in);
     System.out.print("Enter Amount to withraw: ");
     double amount = scanner.nextDouble();
 
@@ -60,6 +57,6 @@ class Main {
       return 0.0;
     }
     return amount; 
-    
+
   }
 }
